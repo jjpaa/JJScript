27206262d5f4dc4e6c7f3bb0e82f16f0da6a6e1d
JJPaa <juhojarvenpaa98@gmail.com>
2025-12-23 09:40:37 +0200

    print work in progress

--- JJScript/Program.cs
+++ JJScript/Program.cs
@@ -11,6 +11,9 @@
         public static long line = 1;
         public static string originalLine = "";
         public static long charCounter = 0;
+        public static string statementString = "";
+        public static bool lineDone = false;
+        public static string modifiedLine = "";
 
         static void Main(string[] args)
         {
@@ -30,73 +33,7 @@
 
                 line = 1;
 
-                IEnumerable<string> lines = File.ReadLines(filePath);
-                charCounter = 0;
-                foreach (var inputLine in lines)
-                {
-                    originalLine = inputLine;
-                    charCounter++;
-                    bool lineDone = false;
-                    string l = inputLine;
-
-                    while (!lineDone)
-                    {
-                        if (l.StartsWith(" ") || l.StartsWith("\t"))
-                        {
-                            l = l.Substring(1);
-                        }
-                        if (l.StartsWith("//"))
-                        {
-                            // This is comment. Skip it
-                            break;
-                        }
-                        if (l.StartsWith("print("))
-                        {
-                            string afterSplit = l.Split("print(").Last();
-                            if (afterSplit.First() == '"')
-                            {
-                                // string starts find where it ends
-                                bool timeToStop = false;
-
-                                foreach (char c in afterSplit)
-                                {
-                                    if (timeToStop == false)
-                                    {
-                                        if (c != '"')
-                                        {
-                                            Console.Write(c);
-                                        }
-                                        else
-                                        {
-                                            //This string is done
-                                            timeToStop = true;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (c == ')')
-                                        {
-                                            // All good
-                                            lineDone = true;
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            throw new SyntaxException() { Expected = ")", Was = c.ToString() };
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                // Not string yet
-                                throw new SyntaxException() { Expected = "\"", Was = afterSplit.First().ToString() };
-                            }
-                        }
-                    }
-
-                    line++;
-                }
+                IterateThroughTheLine(filePath);
             }
             catch (SyntaxException ex)
             {
@@ -108,5 +45,133 @@
                 Console.WriteLine(ex.ToString());
             }
         }
+
+        static void IterateThroughTheLine(string filePath)
+        {
+            IEnumerable<string> lines = File.ReadLines(filePath);
+            foreach (var inputLine in lines)
+            {
+                charCounter = 0;
+                originalLine = inputLine;
+                lineDone = false;
+                modifiedLine = inputLine;
+
+                while (!lineDone)
+                {
+                    charCounter++;
+                    // Check escape chars
+
+                    if (modifiedLine == "")
+                    {
+                        lineDone = true;
+                        break;
+                    }
+
+                    var firstChar = modifiedLine[0];
+
+                    switch(firstChar)
+                    {
+                        // Escpace characters
+                        case '\\':
+                            switch (modifiedLine[1])
+                            {
+                                case '\\':
+                                    // Backslash?
+                                    break;
+                                case '\t':
+                                    // tab
+                                    modifiedLine = modifiedLine.Substring(2);
+                                    break;
+                                case '\n':
+                                    // new line
+                                    break;
+                                case '\"':
+                                    // double quete
+                                    break;
+                                case '\'':
+                                    // double quete
+                                    break;
+                            }
+                            break;
+                        case ';':
+                            // Statement done?
+                            HandleStatement();
+                            break;
+                        case ' ':
+                            modifiedLine = modifiedLine.Substring(1);
+                            break;
+                        case '/':
+                            if (modifiedLine[1] == '/')
+                            {
+                                // Rest is comment. Skip
+                                lineDone = true;
+                                break;
+                            }
+                            throw new SyntaxException() { Expected = "/", Was = modifiedLine[1].ToString() };
+                        default:
+                            // Put values into statement
+                            statementString += firstChar;
+                            modifiedLine = modifiedLine.Substring(1);
+                            break;
+                    }
+                }
+                line++;
+            }
+        }
+
+        private static void HandleStatement()
+        {
+            // This is statement
+            if (modifiedLine.StartsWith("print("))
+            {
+                string afterSplit = modifiedLine.Split("print(").Last();
+                if (afterSplit.First() == '"')
+                {
+                    // string starts find where it ends
+                    bool timeToStop = false;
+
+                    foreach (char c in afterSplit)
+                    {
+                        if (timeToStop == false)
+                        {
+                            if (c != '"')
+                            {
+                                Console.Write(c);
+                            }
+                            else
+                            {
+                                //This string is done
+                                timeToStop = true;
+                            }
+                        }
+                        else
+                        {
+                            if (c == ')')
+                            {
+                                // All good
+                                lineDone = true;
+                                break;
+                            }
+                            else
+                            {
+                                throw new SyntaxException() { Expected = ")", Was = c.ToString() };
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // Not string yet
+                    throw new SyntaxException() { Expected = "\"", Was = afterSplit.First().ToString() };
+                }
+            }
+            else
+            {
+                throw new SyntaxException() { Expected = "Valid statement", Was = statementString };
+            }
+        }
+
+
+
     }
 }
